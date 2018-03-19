38359e3ecefaa93cfeb5f223e2719d40b3043112
sinchang <sinchang@DESKTOP-MCV7TCV>
2018-03-19 20:14:04 +0800

    new

---
+++ EditorPersonalProject/EditorPersonalProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EditorPersonalProject.MvcApplication" Language="C#" %>
---
+++ EditorPersonalProject/EditorPersonalProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EditorPersonalProject.Startup))]
+namespace EditorPersonalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
